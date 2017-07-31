af872c143e81bf8875b6ac904cf36edad89bace2
andrej <lednicky.andrej@gmail.com>
2017-07-31 21:23:21 +0200

    update

--- app.js
+++ app.js
@@ -16,41 +16,41 @@ app.get('/', function(req, res){
 
 app.get('/webhook', function(req, res) {
 
-    if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === 'verify_token_aaa') {
-        console.log("Validating webhook");
-        res.status(200).send(req.query['hub.challenge']);
-    } else {
-        console.error("Failed validation. Make sure the validation tokens match.");
-        res.sendStatus(403);
-    }
-
-    // var data = req.body;
-    // console.log(data);
-    // if (data.object === 'page') {
-    //
-    //     // Iterate over each entry - there may be multiple if batched
-    //     data.entry.forEach(function(entry) {
-    //         var pageID = entry.id;
-    //         var timeOfEvent = entry.time;
-    //
-    //         // Iterate over each messaging event
-    //         entry.messaging.forEach(function(event) {
-    //             if (event.message) {
-    //                 receivedMessage(event);
-    //             } else {
-    //                 console.log("Webhook received unknown event: ", event);
-    //             }
-    //         });
-    //     });
-    //
-    //     // Assume all went well.
-    //     //
-    //     // You must send back a 200, within 20 seconds, to let us know
-    //     // you've successfully received the callback. Otherwise, the request
-    //     // will time out and we will keep trying to resend.
-    //     res.sendStatus(200);
+    // if (req.query['hub.mode'] === 'subscribe' &&
+    //     req.query['hub.verify_token'] === 'verify_token_aaa') {
+    //     console.log("Validating webhook");
+    //     res.status(200).send(req.query['hub.challenge']);
+    // } else {
+    //     console.error("Failed validation. Make sure the validation tokens match.");
+    //     res.sendStatus(403);
     // }
+
+    var data = req.body;
+    console.log(data);
+    if (data.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function(entry) {
+            var pageID = entry.id;
+            var timeOfEvent = entry.time;
+
+            // Iterate over each messaging event
+            entry.messaging.forEach(function(event) {
+                if (event.message) {
+                    receivedMessage(event);
+                } else {
+                    console.log("Webhook received unknown event: ", event);
+                }
+            });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know
+        // you've successfully received the callback. Otherwise, the request
+        // will time out and we will keep trying to resend.
+        res.sendStatus(200);
+    }
 
 });
 
